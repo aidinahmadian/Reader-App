4aeeba069aae9335707d2bb02e7e0546546332da
Aidin Ahmadian <ahmadian_aidin@yahoo.com>
2019-12-03 14:43:55 +0330

    Initial Commit

---
+++ Sign Up-Sign in/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sign Up-Sign in
+//
+//  Created by aidin ahmadian on 12/3/19.
+//  Copyright © 2019 aidin ahmadian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sign Up-Sign inTests/Sign_Up_Sign_inTests.swift
@@ -0,0 +1,34 @@
+//
+//  Sign_Up_Sign_inTests.swift
+//  Sign Up-Sign inTests
+//
+//  Created by aidin ahmadian on 12/3/19.
+//  Copyright © 2019 aidin ahmadian. All rights reserved.
+//
+
+import XCTest
+@testable import Sign_Up_Sign_in
+
+class Sign_Up_Sign_inTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
